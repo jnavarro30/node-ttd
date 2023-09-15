87de14536da6186b0de6bad0a27da8dd533b71d5
jnavarro30 <jnavarro30@live.com>
2023-09-15 16:10:18 -0700

    add and test database

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./src/app');
+const sequelize = require('./src/config/database');
+
+sequelize.sync();
+
+app.listen(3000, () => {
+  console.log('App is runiing on PORT 3000');
+});
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const User = require('./user/User');
+
+app.use(express.json());
+
+app.post('/api/1.0/users', (req, res) => {
+  User.create(req.body).then(() => {
+    return res.send({ message: 'User created' });
+  })
+});
+
+module.exports = app;
