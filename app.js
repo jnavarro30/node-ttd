bc0678c3e1d975318ae4b1db1e11d448051605b0
jnavarro30 <jnavarro30@live.com>
2023-09-15 08:48:49 -0700

    add formatter and linter

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
-    console.log(`App is runiing on PORT: 3000`);
-})
+  console.log('App is runiing on PORT 3000');
+});
